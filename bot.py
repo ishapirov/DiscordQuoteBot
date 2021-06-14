cd77f599bb5f2b6956fd285ba427b6a8558cc035
ishapirov <47495391+ishapirov@users.noreply.github.com>
2021-06-14 14:55:50 -0400

    added new commands

--- bot.py
+++ bot.py
@@ -8,12 +8,13 @@ GUILD = os.environ['DISCORD_GUILD']
 COMMAND_PREFIX = '!'
 BOT_NAME = 'QuoteBot'
 
-HOST=os.environ['HOST']
+HOST=os.environ['DATABASE_URL']
 DATABASE=os.environ['DATABASE']
 USERNAME=os.environ['USERNAME']
 PASSWORD=os.environ['PASSWORD']
 
 DB_TABLE = 'quotes'
+DB_COL_QID = 'quote_id'
 DB_COL_AUTHOR = 'author'
 DB_COL_QUOTE = 'quote_said'
 
@@ -41,31 +42,47 @@ async def on_message(message):
 async def quote(ctx):
     row = selectAnyQuote()
     if row != None:
-        author = row[0]
-        quote = row[1]
-        formattedResponse = '"' + quote + '"' + " - " + author
+        formattedResponse = formatQuote(row)
         await ctx.send(formattedResponse)
     else:
         await ctx.send("No quotes have been added yet.")
- 
-        
 
 @bot.command(name='quotefrom', help='Responds with a random quote from the specified user')
 async def quote(ctx,quote_author):
     if checkPersonHasQuote(quote_author.lower()):
         row = selectPersonQuote(quote_author.lower())
-        author = row[0]
-        quote = row[1]
-        formattedResponse = '"' + quote + '"' + " - " + author
+        formattedResponse = formatQuote(row)
         await ctx.send(formattedResponse)
     else:
         await ctx.send("No quotes have been added for this person yet.")
 
+@bot.command(name="delete", help="Deletes a quote with the given id")
+async def delete_quote(ctx, quote_id : int):
+    if(selectQuoteByID(quote_id) == None):
+        ctx.send("A quote with the given id could not be found.")
+    else:
+        deleteQuoteByID(quote_id)
+        ctx.send("The quote was successfully deleted!")
+
+@bot.event
+async def on_message_edit(before, after):
+    prevQuote = validateQuoteFormat(before.content)
+    if(prevQuote != None):
+        deleteQuoteByQuoteAndAuthor(prevQuote[0],prevQuote[1])
+    newQuote = validateQuoteFormat(after.content)
+    if(newQuote != None):
+        insertQuote(newQuote[0],newQuote[1])
+
+@bot.event
+async def on_message_delete(message):
+    prevQuote = validateQuoteFormat(message.content)
+    if(prevQuote != None):
+        deleteQuoteByQuoteAndAuthor(prevQuote[0],prevQuote[1])
 
 # @bot.command(name='addhistory',help="Adds quotes from the message history of this channel. If used multiple times, the same quotes will be readded")
 # @commands.has_role('OG Loser Bois')
 async def addHistoricalQuotes(ctx):
-    messages = await ctx.channel.history(limit=1000).flatten()
+    messages = await ctx.channel.history(limit=2000).flatten()
     for message in messages:
         quote = validateQuoteFormat(message.content)
         if(quote != None):
@@ -89,6 +106,13 @@ def validateQuoteFormat(message):
     author = author.replace("'","''")
     return (quote,author)
 
+
+def formatQuote(row):
+    quote_id = row[0]
+    author = row[1]
+    quote = row[2]
+    return f"({quote_id}) \"{quote}\" - {author}"
+    
 def establishDBConnection():
     return  psycopg2.connect(
     host=HOST,
@@ -103,10 +127,18 @@ def insertQuote(quote,author):
     conn.commit()
     conn.close()
 
+def selectQuoteByID(quote_id : int):
+    conn = establishDBConnection()
+    cur = conn.cursor()
+    cur.execute(f"SELECT {DB_COL_QID},{DB_COL_AUTHOR},{DB_COL_QUOTE} FROM {DB_TABLE} WHERE {DB_COL_QID} = {quote_id};")
+    row = cur.fetchone()
+    conn.close()
+    return row
+
 def selectAnyQuote():
     conn = establishDBConnection()
     cur = conn.cursor()
-    cur.execute(f"SELECT {DB_COL_AUTHOR},{DB_COL_QUOTE} FROM {DB_TABLE} ORDER BY random() LIMIT 1;")
+    cur.execute(f"SELECT {DB_COL_QID},{DB_COL_AUTHOR},{DB_COL_QUOTE} FROM {DB_TABLE} ORDER BY random() LIMIT 1;")
     row = cur.fetchone()
     conn.close()
     return row
@@ -123,10 +155,25 @@ def checkPersonHasQuote(author):
 def selectPersonQuote(author):
     conn = establishDBConnection()
     cur = conn.cursor()
-    cur.execute(f"SELECT {DB_COL_AUTHOR},{DB_COL_QUOTE} FROM {DB_TABLE} WHERE {DB_COL_AUTHOR} = \'{author}\' ORDER BY random() LIMIT 1;")
+    cur.execute(f"SELECT {DB_COL_QID},{DB_COL_AUTHOR},{DB_COL_QUOTE} FROM {DB_TABLE} WHERE {DB_COL_AUTHOR} = \'{author}\' ORDER BY random() LIMIT 1;")
     row = cur.fetchone()
     conn.close()
     return row
+
+def deleteQuoteByQuoteAndAuthor(quote,author):
+    conn = establishDBConnection()
+    cur = conn.cursor()
+    cur.execute(f"DELETE FROM {DB_TABLE} WHERE {DB_COL_QUOTE} = \'{quote}\' AND {DB_COL_AUTHOR} = \'{author}\';")
+    conn.commit()
+    conn.close()
+
+def deleteQuoteByID(quote_id):
+    conn = establishDBConnection()
+    cur = conn.cursor()
+    cur.execute(f"DELETE FROM {DB_TABLE} WHERE {DB_COL_QID} = {quote_id};")
+    conn.commit()
+    conn.close()
+
 
 
 bot.run(TOKEN)
